b903d5fd29cdd8dcbd2098614bf96b8263b8a77c
mmajic03 <monikamajic03@gmail.com>
2024-11-11 00:54:06 +0100

    Next.js - Deploying Application

---
+++ app/Profile/ProfileSettings/page.tsx
@@ -0,0 +1,8 @@
+export default function ProfileSettings() {
+    return (
+      <main className="flex min-h-screen flex-col items-center p-10">
+        <h1 className="text-6xl font-extrabold tracking-tight">Profile settings</h1>
+      </main>
+    );
+  }
+  
---
+++ app/components/navigation.tsx
@@ -0,0 +1,63 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type Page = {
+  title: string;
+  path: `/${string}`;
+};
+
+
+const pages: Page[] = [
+  { title: "Home", path: "/" },
+  {
+    title: "Recipes",
+    path: "/recipes",
+  },
+  {
+    title: "Inspiration",
+    path: "/inspiration",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "My profile",
+    path: "/Profile",
+  }
+];
+
+function processPage(page: Page, index: number, pathname: string) {
+  return (
+    <li key={index}>
+      <Link
+        href={page.path}
+        className={
+          page.path === "/"
+            ? pathname === page.path
+              ? "font-extrabold"
+              : ""
+            : pathname.startsWith(page.path)
+            ? "font-extrabold"
+            : ""
+        }
+      >
+        {page.title}
+      </Link>
+    </li>
+  );
+}
+
+export function Navigation() {
+    const pathname = usePathname();
+  return (
+    <ul className="flex justify-center space-x-4 mt-8">
+      {pages.map((page, index) => processPage(page, index, pathname))}
+    </ul>
+  );
+}
