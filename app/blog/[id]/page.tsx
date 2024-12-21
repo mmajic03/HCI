18a6afda96d1cf66a9436e553764abf9606a7f92
mmajic03 <monikamajic03@gmail.com>
2024-12-21 15:42:06 +0100

    add CSS to dynamic blog post

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -19,24 +19,30 @@ export default async function BlogPage({ params }: BlogPageProps) {
   console.log(blogPost);
   return (
     <>    
-      <div>
-        <div>
+      <div className="bg-[#A3967C] min-h-screen flex items-center justify-center">
+        <div className="w-11/12 max-w-4xl mx-auto mt-10 p-6 bg-white shadow-md rounded-lg">
+        <h1 className="text-4xl font-bold text-gray-800 mb-6 text-center">{blogPost.name}</h1>
           <img
             src={blogPost.image}
             alt={blogPost.name}
+            className="w-full h-67 object-contain rounded-t-lg"
           />
-          <h1>
-            {blogPost.name}
-          </h1>
-          <p>
-            {blogPost.ingredients.join(", ")}
-          </p>
-          <h2>Instructions</h2>
-          <ol>
+          <div className="p-6">
+            <p className="mt-4 text-grey-800">
+              <span className="font-semibold text-black">Ingredients:</span>
+              <ul className="mt-4 list-disc list-inside text-gray-700 space-y-2">
+                {blogPost.ingredients.map((ingredient, index) => (
+                  <li key={index} className="list-none">{ingredient}</li>
+                ))}
+              </ul>
+            </p>
+          <h2 className="mt-6 text-xl font-semibold text-black">Instructions</h2>
+          <ol className="mt-4 list-decimal list-inside text-gray-700 space-y-2">
             {blogPost.instructions.map((instruction, index) => (
               <li key={index}>{instruction}</li>
             ))}
           </ol>
+          </div>
         </div>
       </div>
     </>
