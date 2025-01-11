db826b4da902b51b54071d5aa0515039b98f3ab5
mmajic03 <monikamajic03@gmail.com>
2025-01-11 09:57:33 +0100

    add hamburger menu to navigation

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 type Page = {
   title: string;
@@ -51,17 +52,44 @@ function processPage(page: Page, index: number, pathname: string) {
 
 export function Navigation() {
   const pathname = usePathname();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <header className="bg-[#70966D] shadow-2xl w-screen py-8 flex items-center justify-between">
       <div className="flex items-center space-x-2">
           <img src="../logoCC.png" className="h-14 w-auto pl-3" />
         </div>
-      <nav className="max-w-7xl mr-8">
+
+      <nav className='hidden md:flex max-w-7xl mr-8'>
         <ul className="flex space-x-12">
           {pages.map((page, index) => processPage(page, index, pathname))}
         </ul>
       </nav>
+      <div className='md:hidden mr-6'>
+        <button   
+          onClick={()=>setIsMenuOpen(!isMenuOpen)}
+          className='flex flex-col justify-between items-center w-10 h-10 p-2 rounded-lg focus:outline-none'
+          >
+          {isMenuOpen ? (
+            <span className='text-white text-2xl font-bold'>X</span>
+          ) : (
+            <>
+              <span className='w-7 h-1 bg-white rounded-full'/>
+              <span className='w-7 h-1 bg-white rounded-full'/>
+              <span className='w-7 h-1 bg-white rounded-full'/>
+            </>
+          )}
+        </button>
+      </div>
+
+      {isMenuOpen && (
+        <nav className='absolute top-[6rem] left-0 w-full bg-[#70966D] shadow-lg md:hidden'>
+          <ul className='flex flex-col space-y-4 p-4'>
+            {pages.map((page, index) => processPage(page, index, pathname))}
+          </ul>
+        </nav>
+      )}
+
     </header>
   );
 }
