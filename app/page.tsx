82b8fedc8b08537d10c2a6d8fde84b015543f4f3
mmajic03 <monikamajic03@gmail.com>
2025-01-14 15:57:32 +0100

    added filters component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Card from "./components/recipeCard";
 import Footer from "./components/footer";
 import PopularRecipes from "./components/PopularRecipes";
 import TopUsers from "./components/TopUsers";
+import Filters from "./components/Filters";
 
 export default function Home() {
   return (
@@ -18,33 +19,7 @@ export default function Home() {
       </div>
 
       <div className="mt-10 flex flex-col lg:flex-row lg:gap-10 w-full">
-        <div className="flex flex-col md:flex-row max-h-[600px] mt-[76px]">
-          <div className="lg:w-full md:w-1/2 sm:w-full bg-[#E4E0D3] p-6 rounded-lg flex flex-col">
-            <h3 className="font-kadwa font-regular text-xl mb-4 border-b border-black">Meals</h3>
-            <ul className="font-kadwa mb-6">
-              <li><input type="checkbox" className="mr-2" />Breakfast</li>
-              <li><input type="checkbox" className="mr-2" />Lunch</li>
-              <li><input type="checkbox" className="mr-2" />Dinner</li>
-              <li><input type="checkbox" className="mr-2" />Soup</li>
-              <li><input type="checkbox" className="mr-2" />Desserts</li>
-              <li><input type="checkbox" className="mr-2" />Snacks</li>
-            </ul>
-            <h3 className="font-kadwa font-regular text-xl mb-4 border-b border-black">Primary ingredient</h3>
-            <ul className="font-kadwa mb-6">
-              <li><input type="checkbox" className="mr-2" />Meat</li>
-              <li><input type="checkbox" className="mr-2" />Fish</li>
-              <li><input type="checkbox" className="mr-2" />Vegetables</li>
-              <li><input type="checkbox" className="mr-2" />Fruits</li>
-              <li><input type="checkbox" className="mr-2" />Pasta</li>
-            </ul>
-            <h3 className="font-kadwa font-regular text-xl mb-4 border-b border-black">Preparation time</h3>
-            <ul className="font-kadwa mb-6">
-              <li><input type="checkbox" className="mr-2" />&lt; 15 min</li>
-              <li><input type="checkbox" className="mr-2" />15 - 45min</li>
-              <li><input type="checkbox" className="mr-2" />&gt; 45 min</li>
-            </ul>
-          </div>
-        </div>
+        <Filters />
         <div className="flex-1">
           <h2 className="text-4xl font-kalam text-white font-bold text-center mb-8 mr-20">Recommended recipes</h2>
           <div>
@@ -57,8 +32,7 @@ export default function Home() {
       <div className="flex space-x-6 mt-5 w-screen bg-[#70966D] p-6 rounded-lg h-[250px] justify-center items-center gap-6">
         <PopularRecipes/>
       </div>
-
-
+      
       <h2 className="text-4xl font-kalam text-white font-bold text-center mt-[60px] mb-5">Top users</h2>
       <TopUsers/>
 
