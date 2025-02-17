1b82f7d8192b2cdcdf598df7337e72f71d311239
amrkonjic <mrkonjicanela@gmail.com>
2025-02-17 15:00:47 +0100

    changing the appearance of the inspiration page

--- app/inspiration/page.tsx
+++ app/inspiration/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Footer from "../components/footer";
+import Image from "next/image";
 
 export default function Inspiration() {
   const kitchens = [
@@ -14,39 +15,30 @@ export default function Inspiration() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-10 bg-[#9C8D71E8] relative">
-      <h1 className="text-6xl font-extrabold tracking-tight">Inspiration</h1>
+      <h1 className="text-6xl font-extrabold tracking-tight mb-12">Inspiration</h1>
 
-      
-        <div className="flex items-center justify-center relative w-screen h-[90vh]">
-          {kitchens.map((kitchen, index) => {
-            const angle = (index / kitchens.length) * 2 * Math.PI;
-            const x = Math.cos(angle) * 350;
-            const y = Math.sin(angle) * 250;
+      <div className="grid grid-cols-1 md:grid-cols-2 mb-12 gap-8 w-full max-w-7xl px-4  ">
+        {kitchens.map((kitchen) => (
+          <div
+            key={kitchen.name}
+            onClick={() => (window.location.href = kitchen.link)}
+            className="cursor-pointer relative w-full aspect-[16/9] shadow-2xl rounded-lg overflow-hidden transition-transform duration-300 hover:scale-105"
+          >
+            <div className="absolute inset-0 bg-gradient-to-r from-black/90 to-transparent flex items-center p-4 z-10">
+              <p className="text-white text-2xl font-bold">{kitchen.name}</p>
+            </div>
 
-            return (
-              <div
-                key={kitchen.name}
-                onClick={() => (window.location.href = kitchen.link)}
-                className="absolute cursor-pointer w-[180px] h-[180px] rounded-full mb-14 mt-10 shadow-2xl flex flex-col items-center justify-center"
-                style={{
-                  transform: `translate(${x}px, ${y}px)`,
-                }}
-              >
-                <img
-                  src={kitchen.img}
-                  alt={kitchen.name}
-                  className="w-full h-full object-cover rounded-full"
-                />
-                <p className="absolute bottom-[-20px] text-center text-sm font-bold">
-                  {kitchen.name}
-                </p>
-              </div>
-            );
-          })}
-        </div>
-     
+            <Image
+              src={kitchen.img}
+              alt={kitchen.name}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        ))}
+      </div>
 
-      <Footer></Footer>
+      <Footer />
     </main>
   );
 }
