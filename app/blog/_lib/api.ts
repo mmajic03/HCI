894d7e0271d5c6700dad5bed45e1bcdf9c353906
mmajic03 <monikamajic03@gmail.com>
2025-03-19 14:18:50 +0100

    modified blog posts

--- app/blog/_lib/api.ts
+++ app/blog/_lib/api.ts
@@ -1,3 +1,6 @@
+import fs from "fs";
+import path from "path";
+
 type PagingInfo = {
     _start?: number;
     _limit?: number;
@@ -11,20 +14,33 @@ export type Post = {
 };
 
 const PAGE_SIZE = Number(process.env.NEXT_PUBLIC_PAGE_SIZE);
+const FILE_PATH = path.join(process.cwd(), "public", "blog_posts.json");
+
+function readLocalPosts(): Post[] {
+    const fileContent = fs.readFileSync(FILE_PATH, "utf-8");
+    return JSON.parse(fileContent);
+}
 
 async function getPostsCount(): Promise<number> {
+    if (typeof window === "undefined") {
+        return readLocalPosts().length;
+    }
+
     const data = await fetch(`${process.env.NEXT_PUBLIC_BLOG_API}/posts/?_limit=1`, {
         method: "HEAD",
     });
     const countHeader = data.headers.get("x-total-count");
-    const count = countHeader ? parseInt(countHeader, 10) : 0; 
-    return count;
+    return countHeader ? parseInt(countHeader, 10) : 0;
 }
 
 async function getPosts({
     _start = 0,
     _limit = PAGE_SIZE,
 }: PagingInfo): Promise<Post[]> {
+    if (typeof window === "undefined") {
+        return readLocalPosts().slice(_start, _start + _limit);
+    }
+
     const data = await fetch(
         `${process.env.NEXT_PUBLIC_BLOG_API}/posts/?_start=${_start}&_limit=${_limit}`
     );
